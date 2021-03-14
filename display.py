ca237f1153e4fc1bd201b88538796fe86d8ef969
Aleksandr Stennikov <a.stennikov@gmail.com>
2021-03-14 09:21:38 +0200

    Created a thread for handling CAN messages, runs much smoother now

--- display.py
+++ display.py
@@ -7,6 +7,7 @@ import can
 import cantools
 from magneticPattern import MagneticPattern
 from encoder import Encoder
+import threading
 
 # creates magnetic pattern
 mp = MagneticPattern()
@@ -45,44 +46,59 @@ encoder1.pid_position_line, = encoder1.pid_axis.plot(0, 0)
 encoder1.pid_position_line.set_xdata(np.arange(encoder1.pid_plot_len))
 encoder1.pid_position_line.set_ydata(encoder1.pid_position_values)
 
-def update(frame):
-    # get messages from bus
+# dictionary that holds raw CAN messages
+sensor_CAN = {0x510: [], 0x511: [], 0x512: [], 0x513: [], 0x514: [], 0x515: []}
+
+# function run in a separate thread, reads can messages and stores them in a dictionary
+program_running = True
+def can_process():
     for msg in bus:
+        sensor_CAN[msg.arbitration_id] = msg    # saves raw message in dictionary, to be processed later
+
+        # unlike sensor messages, messages containing pid information must be processed
         if msg.arbitration_id == 0x510:
             dict = db.decode_message(msg.arbitration_id, msg.data)
             encoder1.update_pid(dict.get('target'), dict.get('position'))
-        if msg.arbitration_id == 0x511:
-            dict = db.decode_message(msg.arbitration_id, msg.data)
-            encoder1.sensor[0] = dict.get('Sensor0')
-            encoder1.sensor[1] = dict.get('Sensor1')
-            encoder1.sensor[2] = dict.get('Sensor2')
-            encoder1.sensor[3] = dict.get('Sensor3')
-        if msg.arbitration_id == 0x512:
-            dict = db.decode_message(msg.arbitration_id, msg.data)
-            encoder1.sensor[4] = dict.get('Sensor4')
-            encoder1.sensor[5] = dict.get('Sensor5')
-            encoder1.sensor[6] = dict.get('Sensor6')
-            encoder1.sensor[7] = dict.get('Sensor7')
-        if msg.arbitration_id == 0x513:
-            dict = db.decode_message(msg.arbitration_id, msg.data)
-            encoder1.sensor[8] = dict.get('Sensor8')
-            encoder1.sensor[9] = dict.get('Sensor9')
-            encoder1.sensor[10] = dict.get('Sensor10')
-            encoder1.sensor[11] = dict.get('Sensor11')
-        if msg.arbitration_id == 0x514:
-            dict = db.decode_message(msg.arbitration_id, msg.data)
-            encoder1.sensor[12] = dict.get('Sensor12')
-            encoder1.sensor[13] = dict.get('Sensor13')
-            encoder1.sensor[14] = dict.get('Sensor14')
-            encoder1.sensor[15] = dict.get('Sensor15')
-        if msg.arbitration_id == 0x515:
-            dict = db.decode_message(msg.arbitration_id, msg.data)
-            encoder1.sensor[16] = dict.get('Sensor16')
-            encoder1.sensor[17] = dict.get('Sensor17')
-            encoder1.sensor[18] = dict.get('Sensor18')
-            encoder1.sensor[19] = dict.get('Sensor19')
-            #print('Target: ' + str(encoder1.target) + ', position: ' + str(encoder1.position))
+
+        if program_running == False:
             break
+
+# create and start can message input thread
+can_msg_input_thread = threading.Thread(target=can_process)
+can_msg_input_thread.start()
+
+
+def update(frame):
+    # update sensor data
+    group1 = db.decode_message(sensor_CAN[0x511].arbitration_id, sensor_CAN[0x511].data)
+    encoder1.sensor[0] = group1.get('Sensor0')
+    encoder1.sensor[1] = group1.get('Sensor1')
+    encoder1.sensor[2] = group1.get('Sensor2')
+    encoder1.sensor[3] = group1.get('Sensor3')
+
+    group2 = db.decode_message(sensor_CAN[0x512].arbitration_id, sensor_CAN[0x512].data)
+    encoder1.sensor[4] = group2.get('Sensor4')
+    encoder1.sensor[5] = group2.get('Sensor5')
+    encoder1.sensor[6] = group2.get('Sensor6')
+    encoder1.sensor[7] = group2.get('Sensor7')
+
+    group3 = db.decode_message(sensor_CAN[0x512].arbitration_id, sensor_CAN[0x512].data)
+    encoder1.sensor[8] = group3.get('Sensor8')
+    encoder1.sensor[9] = group3.get('Sensor9')
+    encoder1.sensor[10] = group3.get('Sensor10')
+    encoder1.sensor[11] = group3.get('Sensor11')
+
+    group4 = db.decode_message(sensor_CAN[0x512].arbitration_id, sensor_CAN[0x512].data)
+    encoder1.sensor[12] = group4.get('Sensor12')
+    encoder1.sensor[13] = group4.get('Sensor13')
+    encoder1.sensor[14] = group4.get('Sensor14')
+    encoder1.sensor[15] = group4.get('Sensor15')
+
+    group5 = db.decode_message(sensor_CAN[0x512].arbitration_id, sensor_CAN[0x512].data)
+    encoder1.sensor[16] = group5.get('Sensor16')
+    encoder1.sensor[17] = group5.get('Sensor17')
+    encoder1.sensor[18] = group5.get('Sensor18')
+    encoder1.sensor[19] = group5.get('Sensor19')
 
     # update plot lines
     encoder1.sensor_line.set_xdata(np.arange(start=encoder1.position, stop = encoder1.position+len(encoder1.sensor)*4, step=4))
@@ -92,21 +108,13 @@ def update(frame):
     encoder1.pid_target_line.set_ydata(encoder1.pid_target_values)
     encoder1.pid_position_line.set_ydata(encoder1.pid_position_values)
 
-    #ax1_line.set_ydata(sensorReadings)
-    #return ax1_line,
     return encoder1.sensor_line, encoder1.sensor_position_line, encoder1.pid_target_line, encoder1.pid_position_line
+
+
 
 ani = FuncAnimation(fig, update, interval=100, blit=True)
 
 plt.tight_layout()
 plt.show()
 
-'''
-ax1.set_xlim(0,20)
-ax1.set_ylim(-2048,2048)
-ax1.set_ylabel('value')
-ax1.set_ylabel('sensor')
-
-ax1_line, = ax1.plot(0, 0)
-ax1_line.set_xdata(xData)
-ax1_line.set_ydata(yData)'''
+program_running = False    # this stops all threads
